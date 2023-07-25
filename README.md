18f7e25d312c53ba56859e1dd053a0f5c345600e
tanzhixuan <tanzhixuan@senseauto.com>
2023-07-25 17:31:22 +0800

    first

---
+++ README.md
@@ -0,0 +1,7 @@
+# tampermonkey-js
+
+包含了国内常用中文网站的篡改猴脚本。有新需求请提 [issue](https://github.com/Tzxhy/tampermonky_js/issues/new). 
+
+## CSDN
+- 允许复制
+
---
+++ js/csdn.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         CSDN优化
+// @namespace    tzxhy
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://blog.csdn.net/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=csdn.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // 允许复制
+    $("#content_views").unbind("copy");
+})();
