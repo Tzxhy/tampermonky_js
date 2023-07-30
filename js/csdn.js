54dd63c177c87c9d7dae0504199be1e86d08960e
Tzxhy <tanzhixuan2018@163.com>
2023-07-30 06:09:39 +0000

    fix: csdn去除登录框

--- js/csdn.js
+++ js/csdn.js
@@ -13,4 +13,32 @@
     'use strict';
     // 允许复制
     $("#content_views").unbind("copy");
+    const targetNode = document.body;
+
+    // 观察器的配置（需要观察什么变动）
+    const config = { attributes: false, childList: true};
+
+    // 当观察到变动时执行的回调函数
+    const callback = function(mutationsList, observer) {
+        // Use traditional 'for loops' for IE 11
+        for(let mutation of mutationsList) {
+            console.log(mutation)
+            if (mutation.type === 'childList') {
+                const loginBoxIdx = [...mutation.addedNodes.values()].findIndex(i => i.classList.contains('passport-login-container'));
+                if (loginBoxIdx >= 0) { // 登录框
+                    const item = mutation.addedNodes[loginBoxIdx];
+                    item.style.setProperty('display', 'none')
+                    setTimeout(() => {
+                        item.remove();
+                    })
+                }
+            }
+        }
+    };
+
+    // 创建一个观察器实例并传入回调函数
+    const observer = new MutationObserver(callback);
+
+    // 以上述配置开始观察目标节点
+    observer.observe(targetNode, config);
 })();
